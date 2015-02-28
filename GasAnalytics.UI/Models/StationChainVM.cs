8bac48e07d95559f6b3e3f2e33d82985ab1887a9
Adam Beilchick <apbeilchick@gmail.com>
2015-02-27 20:40:59 -0500

    Added StationChainVM and StationVM classes and GasAnalytics.BL project

---
+++ GasAnalytics.UI/Models/StationChainVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GasAnalytics.UI.Models
+{
+    public class StationChainVM
+    {
+        [DisplayName("Station Chain Id")]
+        public int StationChainId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public virtual ICollection<StationVM> Stations { get; set; }
+    }
+}
---
+++ GasAnalytics.UI/Models/StationVM.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GasAnalytics.UI.Models
+{
+    public class StationVM
+    {
+        [DisplayName("Station Id")]
+        public int StationId { get; set; }
+
+        [Required]
+        [DisplayName("Station Chain Id")]
+        public int StationChainId { get; set; }
+
+        [DisplayName("Station Number")]
+        public int StationNum { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [DisplayName("Adddress 1")]
+        public string Address1 { get; set; }
+
+        [DisplayName("Address 2 (Optional)")]
+        public string Address2 { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        [MinLength(2)]
+        [MaxLength(2)]
+        public string State { get; set; }
+
+        [Required]
+        [DataType(DataType.PostalCode)]
+        public int Zip { get; set; }
+
+        [Required]
+        [RegularExpression(@"((\(\d{3}\) ?)|(\d{3}-))?\d{3}-\d{4}", ErrorMessage = "Invalid Phone Number!")]
+        public string Phone { get; set; }
+
+        public virtual StationChainVM StationChain { get; set; }
+    }
+}
